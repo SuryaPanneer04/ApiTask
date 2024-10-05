867dd94bada8e3f9d3e5b090890258f70407c55b
Surya <suryapanneer04@gmail.com>
2024-10-05 18:39:41 +0530

    update to Hashing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require('mysql');
 const cors = require('cors');
-
+// const bycrpt = require('bcrypt');
 const app =express();
 
 app.use(cors());
@@ -22,6 +22,7 @@ app.post('/signup',(req,res) =>{
     req.body.password,
     req.body.role
     ]
+    
     db.query(sql, [values],(err, data) => {
         if(err){
              return res.json("Error");
